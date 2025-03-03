eeea5700ed40d05c6cefa434466a04540e05e310
jacobG2026 <jacob.george2026@gbwl.org>
2025-03-03 17:11:38 +0000

    Hiya

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+/* This is a MULTI-LINE commment in JS
+Below is an example of a JS STATEMENT
+(instruction/command
+Statements ends with a SEMICOLON (;w)
+*/
+console.log("Hi");
+
+//This is a SINGLE-LINE comment
+//console.log is a FUNCTIOn to display information
+console.log("this is the console/output");
+
+//DECLARE a VARIABLE to hold data
+// use the LET keyword to create the variable
+let pokemon;
+
+//ASSIGN a VALUE to that varuiable
+//like packing an item into a boz
+pokemon="Pikachu" ;
+
+// Can DECLARE + ASSIGN in the same statement
+let name = "Jacob";
